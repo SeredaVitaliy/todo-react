4f590b639cecd5e131e3f5b1eb27f37c12e3f71a
Sereda Vitaliy <sereda.v484@gmail.com>
2025-11-11 17:11:43 +0200

    Добавлена статистика

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ export function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggeItem}
       />
+      <Stats item={item} />
     </div>
   );
 }
@@ -113,5 +114,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
         Х
       </button>
     </li>
+  );
+}
+
+function Stats({ item }) {
+  if (!item.length)
+    return (
+      <p className="footer">
+        <em>Начни добавлять задачи</em>
+      </p>
+    );
+
+  const numItems = item.length;
+  const numCompleted = item.filter((item) => item.completed).length;
+  const percentage = Math.round((numCompleted / numItems) * 100);
+  return (
+    <div className="footer">
+      <em>
+        {percentage === 100
+          ? "Ты молодец, все сделано"
+          : `Задач всего: ${numItems}. Сделано: ${numCompleted} (${percentage}%)`}
+      </em>
+    </div>
   );
 }
