0d8ed23ccd5adc6e36d7b383cfcf3c5a787472ff
Sereda Vitaliy <sereda.v484@gmail.com>
2025-11-12 09:00:40 +0200

    Добавлен функционал сортировки

--- src/App.jsx
+++ src/App.jsx
@@ -82,20 +82,47 @@ function Form({ onAddItems }) {
 }
 
 function Itemlist({ item, onDeleteItem, onToggleItem }) {
-  return (
-    <>
-      <ul className="todo-list">
-        {item.map((item) => (
-          <Item
-            item={item}
-            key={item.id}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
-          />
-        ))}
-      </ul>
-    </>
-  );
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = item;
+
+  if (sortBy === "active")
+    sortedItems = item.slice().sort((a, b) => a.active.localeCompare(b.active));
+
+  if (sortBy === "completed")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => Number(a.completed) - Number(b.completed));
+
+  if (sortedItems.length === 0) return <></>;
+  if (sortedItems.length > 0)
+    return (
+      <>
+        <ul className="todo-list">
+          {sortedItems.map((item) => (
+            <Item
+              item={item}
+              key={item.id}
+              onDeleteItem={onDeleteItem}
+              onToggleItem={onToggleItem}
+            />
+          ))}
+        </ul>
+        <div className="todo-controls">
+          <select
+            value={sortBy}
+            onChange={(e) => setSortBy(e.target.value)}
+            className="todo-select"
+          >
+            <option value="input">Сортировка по добавлению</option>
+            <option value="active">Сортировка по алфавиту</option>
+            <option value="completed">Сотировка по выполнению</option>
+          </select>
+          <button className="todo-reset">Очистить поле</button>
+        </div>
+      </>
+    );
 }
 
 function Item({ item, onDeleteItem, onToggleItem }) {
